f87f4bc0780bbb7ea1b2952681ac58a4db3ffabf
ayodeji <ayodeji.ogundare@adyen.com>
2022-11-24 15:41:08 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ if [[ -z ${APP_URL+x} ]] ; then
 fi
 
 # Setup domain
-sudo docker exec shopware bash -c "bin/console sales-channel:update:domain ${APP_URL}"
+ocker exec shopware bash -c "bin/console sales-channel:update:domain ${APP_URL}"
 
 # Install Adyen Plugin 
-sudo docker exec shopware bash -c 'composer require adyen/adyen-shopware6'
-sudo docker exec shopware bash -c 'php bin/console plugin:refresh'
-sudo docker exec shopware bash -c 'php bin/console plugin:install AdyenPaymentShopware6 --activate'
-sudo docker exec shopware bash -c 'php bin/console cache:clear'
+docker exec shopware bash -c 'composer require adyen/adyen-shopware6'
+docker exec shopware bash -c 'php bin/console plugin:refresh'
+docker exec shopware bash -c 'php bin/console plugin:install AdyenPaymentShopware6 --activate'
+docker exec shopware bash -c 'php bin/console cache:clear'
 
 # Upgrade HTTP to HTTPS
 CSP='Header set Content-Security-Policy "upgrade-insecure-requests"'
-sudo docker exec shopware bash -c "echo -e '\n${CSP}' >> /var/www/html/public/.htaccess"
+docker exec shopware bash -c "echo -e '\n${CSP}' >> /var/www/html/public/.htaccess"
